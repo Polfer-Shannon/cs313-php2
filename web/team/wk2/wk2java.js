7142011a05e6763c8d05f39aaf2dc4c5228cfb81
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-01 13:56:36 -0600

    team folder and wk1 files

---
+++ web/team/wk2/wk2java.js
@@ -0,0 +1,27 @@
+function clicked() {
+alert("Clicked");
+        }
+
+function changeColor() {
+var textbox_id = "txtColor";
+        var textbox = document.getElementById(textbox_id)
+
+        var div_id = "div1";
+        var div = document.getElementById(div_id);
+        //verify values
+        var color = textbox.value;
+        div.style.backgroundColor = color;
+        }
+
+
+            $(document).ready(function () {
+                $("#btn2").click(function () {
+                    var $colorJQ = $("#txtColorJQ").val();
+                    $("#div2").css("background-color", $colorJQ);
+                });
+                          
+                
+            });
+            
+
+
