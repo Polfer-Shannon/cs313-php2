e8200200d313a28e47149cd8986235b73e873332
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-01 15:42:32 -0600

    finished all but stretch challange #3

--- web/team/wk2/wk2java.js
+++ web/team/wk2/wk2java.js
@@ -1,27 +1,34 @@
 function clicked() {
-alert("Clicked");
-        }
+    alert("Clicked");
+}
 
 function changeColor() {
-var textbox_id = "txtColor";
-        var textbox = document.getElementById(textbox_id)
+    var textbox_id = "txtColor";
+    var textbox = document.getElementById(textbox_id)
 
-        var div_id = "div1";
-        var div = document.getElementById(div_id);
-        //verify values
-        var color = textbox.value;
-        div.style.backgroundColor = color;
-        }
+    var div_id = "div1";
+    var div = document.getElementById(div_id);
+    //verify values
+    var color = textbox.value;
+    div.style.backgroundColor = color;
+}
 
 
-            $(document).ready(function () {
-                $("#btn2").click(function () {
-                    var $colorJQ = $("#txtColorJQ").val();
-                    $("#div2").css("background-color", $colorJQ);
-                });
-                          
-                
-            });
-            
+$(document).ready(function () {
+    $("#btn2").click(function () {
+        var $colorJQ = $("#txtColorJQ").val();
+        $("#div2").css("background-color", $colorJQ);
+    });
+
+
+});
+
+$(document).ready(function () {
+    $("#btn3").click(function () {
+        $("#div3").fadeToggle("slow");
+    });
+
+
+});
 
 
