43ccbd9fc0e933818fd214c86ffc97601c825ad7
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-01 21:08:34 -0600

    finished

--- web/team/wk2/wk2java.js
+++ web/team/wk2/wk2java.js
@@ -3,11 +3,11 @@ function clicked() {
 }
 
 function changeColor() {
-    var textbox_id = "txtColor";
-    var textbox = document.getElementById(textbox_id)
+//    var textbox_id = "txtColor";
+    var textbox = document.getElementById("txtColor");
 
-    var div_id = "div1";
-    var div = document.getElementById(div_id);
+//    var div_id = "div1";
+    var div = document.getElementById("div1");
     //verify values
     var color = textbox.value;
     div.style.backgroundColor = color;
