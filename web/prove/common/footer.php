cefc93f836d32791dc31588b64e9768409aa2a0c
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-02 14:25:59 -0600

    adding prove folder and personal page

---
+++ web/prove/common/footer.php
@@ -0,0 +1,4 @@
+<div class="foot">
+    <p>&copy; Shannon Polfer, All rights reserved.</p>
+    <p>All images used are the property of Shannon Polfer, and/or are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.<p>
+</div>
---
+++ web/prove/views/assignments.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Personal Home Page
+-->
+<html lang="en-us">
+    <head>   
+        <meta charset="UTF-8"/>
+        <meta name="author" content="Shannon Polfer">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SP Home</title>
+        <link href="../css/home_style.css" rel="stylesheet" type="text/css" 
+              media="screen">
+    </head>
+    <body>
+        <header class="header__pages">
+            <?php include ('../common/header.php'); ?> 
+        </header> 
+        <footer>
+            <?php include ('../common/footer.php'); ?> 
+        </footer>
+    </body>
+</html>
+
