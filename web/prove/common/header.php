c2e8ca0f0c1d2dff537fa9aff56c665958ad46f6
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-02 17:16:07 -0600

    finished prove Personal Homepage

--- web/prove/common/header.php
+++ web/prove/common/header.php
@@ -1,11 +1,11 @@
         
-            <div class="header__text-box header__text-box--pages">
-                <h1 class="heading-primary pages">
-                    <span class="heading-primary--main pages">Assignments</span>
-                    <span class="heading-primary--sub pages">Under Construction</span>
-                </h1>
+<div class="header__text-box header__text-box--pages">
+    <h1 class="heading-primary pages">
+        <span class="heading-primary--main pages">Assignments</span>
+        <span class="heading-primary--sub pages">Under Construction</span>
+    </h1>
 
-                <a href="../home.php" class="btn btn--white btn--animated btn__pages">&nbsp;&nbsp;&nbsp;&nbsp;Home&nbsp;&nbsp;&nbsp;&nbsp;</a>
-                
-            </div>
+    <a href="../home.php" class="btn btn--white btn--animated btn__pages">&nbsp;&nbsp;&nbsp;&nbsp;Home&nbsp;&nbsp;&nbsp;&nbsp;</a>
+
+</div>
 
