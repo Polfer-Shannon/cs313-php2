bb737dd259790f40d66822196d81c9c0aec81099
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-04 00:09:22 -0600

    updated team assignment

--- web/team/wk3/form.php
+++ web/team/wk3/form.php
@@ -6,14 +6,16 @@
         <link rel="stylesheet" type="text/css" href="form_style.css"> 
     </head>
     <body>
+<!--        stretch-->
         <?php
-        $major = array(
-            "(CS)" => "Computer Science",
-            "(WD)" => "Web Design and Development",
-            "(CIT)" => "Computer Information Technology",
-            "(CE)" => "Computer Engineering",
-        );
-        ?>
+//        $major = array(
+//            "(CS)" => "Computer Science",
+//            "(WDD)" => "Web Design and Development",
+//            "(CIT)" => "Computer Information Technology",
+//            "(CE)" => "Computer Engineering",
+//        );
+//        ?>
+        
         <form method="post" action="student.php">
             <label for="name">Name:</label>
             <input type="text" name="name">
@@ -24,25 +26,39 @@
             <br>
             <label for="major">Major:</label>
             <br>
-            <?php
-            asort($major);
-            foreach ($major as $key => $major_print) {
-                echo "<input type='radio' name='major' value='$major_print'>" . $major_print . ', ' . $key . '<br>';
-            }
-            ?>
+            <input type="radio" name="major" value="Computer Science">Computer Science<br>
+            <input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
+            <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
+            <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+<!--            stretch-->
+            //<?php
+//            asort($major);
+//            foreach ($major as $key => $major_print) {
+//                echo "<input type='radio' name='major' value='$major_print $key'>" . $major_print . ', ' . '<br>';
+//            }
+//            ?>
             <br>
             <label for="comment">Comments:</label>
             <textarea rows="4" cols="50" name="comment"></textarea>
             <br>
             <br>
             <label for="continent">What continents have you visited?</label><br>
-            <input type="checkbox" name="continent[]" value="NA">North America<br>
+          
+            <input type="checkbox" name="continent[]" value="North America">North America<br>
+            <input type="checkbox" name="continent[]" value="South America">South America<br>
+            <input type="checkbox" name="continent[]" value="Europe">Europe<br>
+            <input type="checkbox" name="continent[]" value="Asia">Asia<br>
+            <input type="checkbox" name="continent[]" value="Australia">Australia<br>
+            <input type="checkbox" name="continent[]" value="Africa">Africa<br>
+            <input type="checkbox" name="continent[]" value="Antarctica">Antarctica<br>
+            
+<!--            <input type="checkbox" name="continent[]" value="NA">North America<br>
             <input type="checkbox" name="continent[]" value="SA">South America<br>
             <input type="checkbox" name="continent[]" value="EU">Europe<br>
             <input type="checkbox" name="continent[]" value="AS">Asia<br>
             <input type="checkbox" name="continent[]" value="AR">Australia<br>
             <input type="checkbox" name="continent[]" value="AF">Africa<br>
-            <input type="checkbox" name="continent[]" value="AT">Antarctica<br>
+            <input type="checkbox" name="continent[]" value="AT">Antarctica<br>-->
 
 
 
--- web/team/wk3/student.php
+++ web/team/wk3/student.php
@@ -1,39 +1,55 @@
-<?php $name = htmlspecialchars($_POST["name"]);
+<?php
+$name = htmlspecialchars($_POST["name"]);
 //$email = htmlspecialchars($_POST["email"]);
 //$major = htmlspecialchars($_POST["major"]);
 //$continent = $_POST["continent"];
 //$comment = htmlspecialchars($_POST["comment"]);
-
 ?>
-
 <html>
+    <head>
+        <title>Week 3 Team Assignment</title>  
+        <meta charset="UTF-8">
+        <meta name="viewport" >
+        <link rel="stylesheet" type="text/css" href="form_style.css"> 
+    </head>
+
     <body>
         <?php
-        $map = array(
-            "NA" => "The continent of North America",
-            "SA" => "South America",
-            "EU" => "Europe",
-            "AS" => "Asia",
-            "AR" => "Australian",
-            "AF" => "Africa",
-            "AT" => "and Antarctica(Why would you go here?)",
-            )
+//        stretch
+//        $map = array(
+//            "NA" => "North America",
+//            "SA" => "South America",
+//            "EU" => "Europe",
+//            "AS" => "Asia",
+//            "AR" => "Australian",
+//            "AF" => "Africa",
+//            "AT" => "Antarctica",
+//                )
         ?>
-        <p>Information for : <?= $name ?></p><br>
-        Your email address is: <?php echo $_POST["email"]; ?><br>
+        <h1>Information for : <?= $name ?></h1><br>
+        Your email address is: <?php
+        $help = $_POST['email'];
+        echo "<a href='mailto:" . $_POST["email"] . "?Subject=" . 'Hello' . "'>$help</a>";
+        ?><br>
+
         Your Major is : <?php echo $_POST["major"]; ?><br>
 
         You commented : <?php echo $_POST["comment"]; ?><br>
+
         You have visited : <?php
-        if (isset($_POST["submit"])) {
-            foreach ($_POST["continent"] as $selected) {
-                foreach ($map as $x => $x_value)
-                    if ($selected === $x)
-                echo $x_value;
-                echo "" . ", ";
-            }
+        foreach ($_POST["continent"] as $selected) {
+            echo $selected . ", ";
         }
-       
+
+//        stretch
+//        if (isset($_POST["submit"])) {
+//            foreach ($_POST["continent"] as $selected) {
+//                foreach ($map as $x => $x_value)
+//                    if ($selected === $x)
+//                        echo $x_value;
+//                echo "" . ", ";
+//            }
+//        }
         ?><br>
 
     </body>
