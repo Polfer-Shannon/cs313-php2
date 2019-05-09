2b41839eaa75b6688469eb9ac6b13fea6607a701
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-08 22:38:40 -0600

    finished playing with wk3 team assignment

--- web/team/wk3/form.php
+++ web/team/wk3/form.php
@@ -8,13 +8,13 @@
     <body>
 <!--        stretch-->
         <?php
-//        $major = array(
-//            "(CS)" => "Computer Science",
-//            "(WDD)" => "Web Design and Development",
-//            "(CIT)" => "Computer Information Technology",
-//            "(CE)" => "Computer Engineering",
-//        );
-//        ?>
+        $major = array(
+            "(CS)" => "Computer Science",
+            "(WDD)" => "Web Design and Development",
+            "(CIT)" => "Computer Information Technology",
+            "(CE)" => "Computer Engineering",
+        );
+        ?>
         
         <form method="post" action="student.php">
             <label for="name">Name:</label>
@@ -26,17 +26,17 @@
             <br>
             <label for="major">Major:</label>
             <br>
-            <input type="radio" name="major" value="Computer Science">Computer Science<br>
+<!--            <input type="radio" name="major" value="Computer Science">Computer Science<br>
             <input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
             <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
-            <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+            <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>-->
 <!--            stretch-->
-            //<?php
-//            asort($major);
-//            foreach ($major as $key => $major_print) {
-//                echo "<input type='radio' name='major' value='$major_print $key'>" . $major_print . ', ' . '<br>';
-//            }
-//            ?>
+            <?php
+            asort($major);
+            foreach ($major as $key => $major_print) {
+                echo "<input type='radio' name='major' value='$major_print'>" . $major_print . ', ' . '<br>';
+            }
+            ?>
             <br>
             <label for="comment">Comments:</label>
             <textarea rows="4" cols="50" name="comment"></textarea>
@@ -44,21 +44,21 @@
             <br>
             <label for="continent">What continents have you visited?</label><br>
           
-            <input type="checkbox" name="continent[]" value="North America">North America<br>
+<!--            <input type="checkbox" name="continent[]" value="North America">North America<br>
             <input type="checkbox" name="continent[]" value="South America">South America<br>
             <input type="checkbox" name="continent[]" value="Europe">Europe<br>
             <input type="checkbox" name="continent[]" value="Asia">Asia<br>
             <input type="checkbox" name="continent[]" value="Australia">Australia<br>
             <input type="checkbox" name="continent[]" value="Africa">Africa<br>
-            <input type="checkbox" name="continent[]" value="Antarctica">Antarctica<br>
+            <input type="checkbox" name="continent[]" value="Antarctica">Antarctica<br>-->
             
-<!--            <input type="checkbox" name="continent[]" value="NA">North America<br>
+            <input type="checkbox" name="continent[]" value="NA">North America<br>
             <input type="checkbox" name="continent[]" value="SA">South America<br>
             <input type="checkbox" name="continent[]" value="EU">Europe<br>
             <input type="checkbox" name="continent[]" value="AS">Asia<br>
             <input type="checkbox" name="continent[]" value="AR">Australia<br>
             <input type="checkbox" name="continent[]" value="AF">Africa<br>
-            <input type="checkbox" name="continent[]" value="AT">Antarctica<br>-->
+            <input type="checkbox" name="continent[]" value="AT">Antarctica<br>
 
 
 
--- web/team/wk3/student.php
+++ web/team/wk3/student.php
@@ -16,15 +16,15 @@ $name = htmlspecialchars($_POST["name"]);
     <body>
         <?php
 //        stretch
-//        $map = array(
-//            "NA" => "North America",
-//            "SA" => "South America",
-//            "EU" => "Europe",
-//            "AS" => "Asia",
-//            "AR" => "Australian",
-//            "AF" => "Africa",
-//            "AT" => "Antarctica",
-//                )
+        $map = array(
+            "NA" => "North America",
+            "SA" => "South America",
+            "EU" => "Europe",
+            "AS" => "Asia",
+            "AR" => "Australian",
+            "AF" => "Africa",
+            "AT" => "Antarctica",
+                )
         ?>
         <h1>Information for : <?= $name ?></h1><br>
         Your email address is: <?php
@@ -37,19 +37,19 @@ $name = htmlspecialchars($_POST["name"]);
         You commented : <?php echo $_POST["comment"]; ?><br>
 
         You have visited : <?php
-        foreach ($_POST["continent"] as $selected) {
-            echo $selected . ", ";
-        }
+//        foreach ($_POST["continent"] as $selected) {
+//            echo $selected . ", ";
+//        }
 
 //        stretch
-//        if (isset($_POST["submit"])) {
-//            foreach ($_POST["continent"] as $selected) {
-//                foreach ($map as $x => $x_value)
-//                    if ($selected === $x)
-//                        echo $x_value;
-//                echo "" . ", ";
-//            }
-//        }
+        if (isset($_POST["submit"])) {
+            foreach ($_POST["continent"] as $selected) {
+                foreach ($map as $x => $x_value)
+                    if ($selected === $x)
+                        echo $x_value;
+                echo "" . ", ";
+            }
+        }
         ?><br>
 
     </body>
