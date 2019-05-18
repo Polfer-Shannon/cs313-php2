fdf602781fd3cdcc8ba0d02467e74c5543722ce2
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-17 22:21:33 -0600

    trying more sql commands

--- db/myDb.sql
+++ db/myDb.sql
@@ -32,8 +32,9 @@ INSERT INTO recipes (name, rank, date, user_id, directions)
 VALUES ('White Bean Chili', 2, '2019-04-28', 1, 'Cook the chicken...');
 
 INSERT INTO ingredients (food, category, on_hand, recipe_id)
-VALUES ('Chicken', 'meat', TRUE, 1),
-('White Beans', 'canned', FALSE, 1);
+VALUES ('Chicken', 'meat', TRUE, 2),
+('White Beans', 'canned', FALSE, 2),
+('Heavy Whipping Cream', 'dairy', FALSE, 2);
 
 SELECT ingredients.food, recipes.name, users.first_name, users.last_name, recipes.rank 
 FROM ingredients
@@ -52,3 +53,26 @@ LEFT JOIN ingredients ON ingredients.on_hand = TRUE;
 UPDATE recipes
 SET directions = 'Saute onions and garlic in olive oil. Place all ingredients in pot.'
 Where recipes.name = 'White Bean Chili';
+
+CREATE TABLE recipes (
+id SERIAL PRIMARY KEY,
+name VARCHAR(20) NOT NULL,
+rank SMALLINT CHECK (rank > 0 AND rank < 6) NOT NULL,
+date DATE NOT NULL,
+user_id INT NOT NULL REFERENCES users(id),
+directions TEXT NOT NULL
+);
+
+DELETE FROM ingredients
+WHERE id = 1;
+
+CREATE VIEW myview AS
+    SELECT name, on_hand
+        FROM recipes, ingredients
+        WHERE on_hand = FALSE;
+
+SELECT * FROM myview;
+
+SELECT food AS Grocery_list
+FROM ingredients
+WHERE on_hand = FALSE;
