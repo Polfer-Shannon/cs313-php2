25558a5dea2595875c54cefb76bafe534445849a
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-21 16:27:23 -0600

    team 5 assignment start

--- web/team/wk5/scriptures.php
+++ web/team/wk5/scriptures.php
@@ -40,6 +40,7 @@ catch (PDOException $ex)
 foreach ($db->query('SELECT * FROM scriptures') as $row)
 {
     echo $row['book'] . $row['chapter'] .':' . $row['verse'] . '-' . '"' . $row['content'] . '"';
+    echo '<br>';
 }
         ?>
     </body>
