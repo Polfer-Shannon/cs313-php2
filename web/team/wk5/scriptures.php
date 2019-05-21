eae000f071459001e77031402b0f41058b5f06fc
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-21 15:47:51 -0600

    team 5 assignment start

---
+++ web/team/wk5/scriptures.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>Week 3 Team Assignment</title>  
+        <meta charset="UTF-8">
+        <meta name="viewport" >
+        <link rel="stylesheet" type="text/css" href="form_style.css"> 
+    </head>
+    <body>
+    <header>
+        <h1>Scripture Resources</h1>
+        
+    </header>
+    
+
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+    </body>
+</html>
