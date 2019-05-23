039c03dedaccd6f0f4c6c3a01f7a936e6b3a8321
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-23 14:51:36 -0600

    Still working on Project1

---
+++ web/prove/views/project1/details.php
@@ -0,0 +1,36 @@
+<?php 
+require 'dbConnect.php';
+$db = get_db();
+?>
+<!DOCTYPE html>
+<!--
+Personal Home Page
+-->
+<html lang="en-us">
+    <head>   
+        <meta charset="UTF-8"/>
+        <meta name="author" content="Shannon Polfer">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SP Home</title>
+        <link href="../../css/home_style.css" rel="stylesheet" type="text/css" 
+              media="screen">
+        <link href="../../css/project1.css" rel="stylesheet" type="text/css" 
+              media="screen">
+        <script src="../../js/homejs.js"></script>
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <?php
+        $recipe_id = $_GET['recipeLinks'];
+        $stmt = $db->prepare('SELECT * FROM scriptures WHERE id=:id');
+        $stmt->bindValue(':id', $recipe_id, PDO::PARAM_STR);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        echo "<p><span>" . $row['name'] . ' '  . "</span>";
+        echo $row['directions'];
+        echo '<br/>';
+        ?>
+    </body>
+</html>
