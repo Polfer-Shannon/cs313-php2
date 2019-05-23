817c3c510f7bb0322d39d011eb6c2f7751accfed
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-23 14:21:12 -0600

    Still working on Project1

--- db/teamWk5.sql
+++ db/teamWk5.sql
@@ -11,3 +11,10 @@ VALUES ('John', 1, 5, 'And the light shineth in darkness; and the darkness compr
 ('Doctrine and Covenants', 88, 49, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.'),
 ('Doctrine and Covenents', 93, 28, 'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.'),
 ('Mosiah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
+
+
+INSERT INTO scriptures (book, chapter, verse, content)
+VALUES ('John', 5, 8, 'Jesus saith unto him, aRise, take up thy bed, and walk.'),
+('Mark', 3, 25, 'And if a house be divided against itself, that house cannot stand.'),
+('Mosiah', 14, 1, 'Yea, even doth not Isaiah say: Who hath abelieved our report, and to whom is the arm of the Lord revealed?'),
+('Mark', 13, 5, 'And Jesus answering them began to say, Take heed lest any man deceive you:');
