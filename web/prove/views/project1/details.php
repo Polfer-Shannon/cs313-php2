7c4e52024741fa0132f0cfe152990066a60aa8bd
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-25 13:27:26 -0600

    working on project1

--- web/prove/views/project1/details.php
+++ web/prove/views/project1/details.php
@@ -33,11 +33,11 @@ Personal Home Page
         $stmt2 = $db->prepare('SELECT ingredients.food FROM ingreidents INNER JOIN (recipes INNER JOIN menu ON recipes.id = menu.recipes_id) ON ingredients.id = menu.ingredients_id WHERE recipes.id=:id');
         $stmt2->bindValue(':id', $recipe_id2, PDO::PARAM_STR);
         $stmt2->execute();
-        $row2 = $stmt->fetch(PDO::FETCH_ASSOC);
+        $row2 = $stmt2->fetch(PDO::FETCH_ASSOC);
         
         echo "<p class='recipe_details'><span>" . $row['name'] . ': '  . 'Ingredient List' . "</span>";
         echo '<br>';
-        echo $row['directions'] . '"' . "</p>";
+        echo $row2['food'] . '"' . "</p>";
         echo '<br/>';
         var_dump($recipe_id2);
         ?>
