64cd70ac82978139ea5a10a82487615a57928d6d
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-25 13:21:24 -0600

    working on project1

--- web/prove/views/project1/details.php
+++ web/prove/views/project1/details.php
@@ -29,15 +29,15 @@ Personal Home Page
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         
-        $recipe_id2 = $_GET['recipeLinks'];
-        $stmt2 = $db->prepare('SELECT ingredients.food FROM ingreidents INNER JOIN (recipes INNER JOIN menu ON recipes.id = menu.recipes_id) ON ingredients.id = menu.ingredients_id WHERE recipes.id=:id');
-        $stmt2->bindValue(':id', $recipe_id, PDO::PARAM_STR);
-        $stmt2->execute();
-        $row2 = $stmt->fetch(PDO::FETCH_ASSOC);
+//        $recipe_id2 = $_GET['recipeLinks'];
+//        $stmt2 = $db->prepare('SELECT ingredients.food FROM ingreidents INNER JOIN (recipes INNER JOIN menu ON recipes.id = menu.recipes_id) ON ingredients.id = menu.ingredients_id WHERE recipes.id=:id');
+//        $stmt2->bindValue(':id', $recipe_id, PDO::PARAM_STR);
+//        $stmt2->execute();
+//        $row2 = $stmt->fetch(PDO::FETCH_ASSOC);
         
         echo "<p class='recipe_details'><span>" . $row['name'] . ': '  . 'Ingredient List' . "</span>";
         echo '<br>';
-        echo $row2['food'] . '"' . "</p>";
+        echo $row['directions'] . '"' . "</p>";
         echo '<br/>';
         ?>
     </body>
