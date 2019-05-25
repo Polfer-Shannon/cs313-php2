c3a010dfa4b244f345102f4c5d3d2ccee3fbfeaa
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-24 18:41:33 -0600

    Working on Project1

--- web/prove/views/project1/p1home.php
+++ web/prove/views/project1/p1home.php
@@ -28,7 +28,7 @@ Personal Home Page
                 <a href="../../../index.php" class="btn btn--white btn--animated btn__pages">&nbsp;&nbsp;&nbsp;&nbsp;Home&nbsp;&nbsp;&nbsp;&nbsp;</a>
             </div>
         </header>  
-        <form method="post" action="results.php">
+        <form method="post" action="p1home.php">
 
             <input type="text" name="username" placeholder="Enter Username">
             <input class="btn btn--white btn--animated" type="submit" value="List Your Recipes">
@@ -36,7 +36,7 @@ Personal Home Page
         </form>
         
         
-        <form method="post" action="p1home.php">
+        <form method="post" action="results.php">
 
             <input type="text" name="recipe" placeholder="Enter id#">
             <input class="btn btn--white btn--animated" type="submit" value="View a Recipe">
