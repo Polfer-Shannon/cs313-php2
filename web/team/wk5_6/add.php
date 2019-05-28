a9cbd7a6342449579d2662ccaddd79520545922a
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-28 17:57:38 -0600

    working on team6 pages

--- web/team/wk5_6/add.php
+++ web/team/wk5_6/add.php
@@ -3,7 +3,7 @@ require 'dbConnect.php';
 $db = get_db();
 ?>
 <?php
-            $add_script = htmlspecialchars($_POST['addScripture']);
+$add_script = htmlspecialchars($_POST['addScripture']);
 //            $stmt = $db->prepare('SELECT * FROM scriptures WHERE book=:book');
 //            $stmt->bindValue(':book', $book, PDO::PARAM_STR);
 //            $stmt->execute();
@@ -15,9 +15,9 @@ $db = get_db();
 //                echo ':' . $r['verse'];
 //                echo '</a></p>';
 //            }
-            
-            echo $add_script;
-            ?>
+
+var_dump($add_script);
+?>
 
 <html>
     <head>
@@ -35,28 +35,28 @@ $db = get_db();
 
         <div>
             <form method="post" action="add.php">
-                
+
                 <label for="add_book">Add a book</label>
                 <input type="text" name="add_book">
-                
+
                 <label for="add_chapter">Add a chapter</label>
                 <input type="text" name="add_chapter">
-                
+
                 <label for="add_verse">Add a verse</label>
                 <input type="text" name="add_verse">
-                
+
                 <label for="add_content">Add content</label>
                 <textarea rows="6" cols="50" name="add_content"></textarea>
-                
-                
+
+
                 <input type="submit" value="addScripture">
 
             </form>
 
 
-            
-            
-            
+
+
+
         </div>
     </body>
 </html>
