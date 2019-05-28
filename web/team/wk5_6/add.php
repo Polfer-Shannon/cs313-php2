61ef75b9f1a06ed4761dba91dc0327dcd25fa7f2
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-28 17:49:48 -0600

    working on team6 pages

--- web/team/wk5_6/add.php
+++ web/team/wk5_6/add.php
@@ -2,7 +2,22 @@
 require 'dbConnect.php';
 $db = get_db();
 ?>
-
+<?php
+            $add_script = htmlspecialchars($_POST['addScripture']);
+            $stmt = $db->prepare('SELECT * FROM scriptures WHERE book=:book');
+//            $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+//            $stmt->execute();
+//            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//            foreach ($rows as $r) {
+//                echo '<p><a href="details.php?scripture=' . $r['id'] . '">';
+//                echo $r['book'] . ' ';
+//                echo $r['chapter'];
+//                echo ':' . $r['verse'];
+//                echo '</a></p>';
+//            }
+            
+            echo $add_script;
+            ?>
 
 <html>
     <head>
@@ -39,22 +54,7 @@ $db = get_db();
             </form>
 
 
-            <?php
-            $add_script = $_POST['addScripture'];
-            $stmt = $db->prepare('SELECT * FROM scriptures WHERE book=:book');
-            $stmt->bindValue(':book', $book, PDO::PARAM_STR);
-            $stmt->execute();
-            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            foreach ($rows as $r) {
-                echo '<p><a href="details.php?scripture=' . $r['id'] . '">';
-                echo $r['book'] . ' ';
-                echo $r['chapter'];
-                echo ':' . $r['verse'];
-                echo '</a></p>';
-            }
             
-            
-            ?>
             
             
         </div>
