644953c16ded4d6a2b00f40b2e15d6a2b68a88cb
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-28 19:37:21 -0600

    working on team6 pages

--- web/team/wk5_6/add.php
+++ web/team/wk5_6/add.php
@@ -37,32 +37,32 @@ $db = get_db();
                 <?php
                 $stmt = $db->query('SELECT all FROM topic');
                 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+                $stmt->execute();
 
                 foreach ($results as $row) {
-                    echo "<input type ='checkbox' value = '$row'>" . $row['name'] . ', ' . '<br>';
+                    echo "<input type ='radio' value = '$row'>" . $row['name'] . '<br>';
                 }
-                    ?>
+                ?>
 
 
-                    <input type="submit" value="Add Scripture">
-                </form>
+                <input type="submit" value="Add Scripture">
+            </form>
 
-            </div>
+        </div>
 
-    <?php
-    $add_book = htmlspecialchars($_POST['add_book']);
-    $add_chapter = htmlspecialchars($_POST['add_chapter']);
-    $add_verse = htmlspecialchars($_POST['add_verse']);
-    $add_content = htmlspecialchars($_POST['add_content']);
+        <?php
+        $add_book = htmlspecialchars($_POST['add_book']);
+        $add_chapter = htmlspecialchars($_POST['add_chapter']);
+        $add_verse = htmlspecialchars($_POST['add_verse']);
+        $add_content = htmlspecialchars($_POST['add_content']);
 
-    $stmt = $db->prepare('INSERT INTO scriptures(book, chapter, verse, content) VALUES(:add_book, :add_chapter, :add_verse, :add_content);');
-    $stmt->bindValue(':add_book', $add_book, PDO::PARAM_STR);
-    $stmt->bindValue(':add_chapter', $add_chapter, PDO::PARAM_INT);
-    $stmt->bindValue(':add_verse', $add_verse, PDO::PARAM_INT);
-    $stmt->bindValue(':add_content', $add_content, PDO::PARAM_STR);
-    $stmt->execute();
-    ?>
+        $stmt = $db->prepare('INSERT INTO scriptures(book, chapter, verse, content) VALUES(:add_book, :add_chapter, :add_verse, :add_content);');
+        $stmt->bindValue(':add_book', $add_book, PDO::PARAM_STR);
+        $stmt->bindValue(':add_chapter', $add_chapter, PDO::PARAM_INT);
+        $stmt->bindValue(':add_verse', $add_verse, PDO::PARAM_INT);
+        $stmt->bindValue(':add_content', $add_content, PDO::PARAM_STR);
+        $stmt->execute();
+        ?>
 
     </body>
 </html>
