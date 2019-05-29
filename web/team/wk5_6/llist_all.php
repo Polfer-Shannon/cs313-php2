30cda6922fbb5a3ee18017256b733f90c44ad79e
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-28 18:14:56 -0600

    working on team6 pages

--- web/team/wk5_6/llist_all.php
+++ web/team/wk5_6/llist_all.php
@@ -15,3 +15,34 @@ $content = htmlspecialchars($_POST['add_content']);
 
 echo $add_script;
 ?>
+<html>
+    <head>
+        <title>Week 6 Team Assignment</title>  
+        <meta charset="UTF-8">
+        <meta name="viewport" >
+        <link rel="stylesheet" type="text/css" href="scriptures.css"> 
+    </head>
+    <body>
+        <header>
+            <h1>Add Scripture and Topic</h1>
+
+        </header>
+                <?php
+$add_script = htmlspecialchars($_POST['addScripture']);
+$content = htmlspecialchars($_POST['add_content']);
+//            $stmt = $db->prepare('SELECT * FROM scriptures WHERE book=:book');
+//            $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+//            $stmt->execute();
+//            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//            foreach ($rows as $r) {
+//                echo '<p><a href="details.php?scripture=' . $r['id'] . '">';
+//                echo $r['book'] . ' ';
+//                echo $r['chapter'];
+//                echo ':' . $r['verse'];
+//                echo '</a></p>';
+//            }
+
+echo $add_script;
+?>
+    </body>
+</html>
