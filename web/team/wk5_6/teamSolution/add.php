515a3855c967658e54ca2c524b8d5e449c6101aa
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-28 21:01:03 -0600

    working on team6 pages

--- web/team/wk5_6/teamSolution/add.php
+++ web/team/wk5_6/teamSolution/add.php
@@ -68,7 +68,8 @@ $db = get_db();
             $stmt2 = $db->prepare('SELECT topic.name FROM topic
         LEFT JOIN scrip_top
         ON scrip_top.topic_id = topic.id
-        WHERE scrip_top.scriptures_id = $scriptureid');
+        WHERE scrip_top.scriptures_id = :scriptureid');
+            $stmt2->bindValue(':scriptureid', $scriptureid, PDO::PARAM_INT);
             $stmt2->execute();
 //            $topics = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 //            foreach ($topics as $t) {
