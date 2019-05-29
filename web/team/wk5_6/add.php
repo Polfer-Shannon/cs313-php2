f0839dd5a778c5dc5c9d8db5d38fd988ee748762
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-28 18:01:25 -0600

    working on team6 pages

--- web/team/wk5_6/add.php
+++ web/team/wk5_6/add.php
@@ -2,22 +2,7 @@
 require 'dbConnect.php';
 $db = get_db();
 ?>
-<?php
-$add_script = htmlspecialchars($_POST['addScripture']);
-//            $stmt = $db->prepare('SELECT * FROM scriptures WHERE book=:book');
-//            $stmt->bindValue(':book', $book, PDO::PARAM_STR);
-//            $stmt->execute();
-//            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//            foreach ($rows as $r) {
-//                echo '<p><a href="details.php?scripture=' . $r['id'] . '">';
-//                echo $r['book'] . ' ';
-//                echo $r['chapter'];
-//                echo ':' . $r['verse'];
-//                echo '</a></p>';
-//            }
 
-var_dump($add_script);
-?>
 
 <html>
     <head>
@@ -36,14 +21,14 @@ var_dump($add_script);
         <div>
             <form method="post" action="add.php">
 
-                <label for="add_book">Add a book</label>
+<!--                <label for="add_book">Add a book</label>
                 <input type="text" name="add_book">
 
                 <label for="add_chapter">Add a chapter</label>
                 <input type="text" name="add_chapter">
 
                 <label for="add_verse">Add a verse</label>
-                <input type="text" name="add_verse">
+                <input type="text" name="add_verse">-->
 
                 <label for="add_content">Add content</label>
                 <textarea rows="6" cols="50" name="add_content"></textarea>
@@ -53,10 +38,24 @@ var_dump($add_script);
 
             </form>
 
-
-
-
-
         </div>
+        
+        <?php
+$add_script = htmlspecialchars($_POST['addScripture']);
+//            $stmt = $db->prepare('SELECT * FROM scriptures WHERE book=:book');
+//            $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+//            $stmt->execute();
+//            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//            foreach ($rows as $r) {
+//                echo '<p><a href="details.php?scripture=' . $r['id'] . '">';
+//                echo $r['book'] . ' ';
+//                echo $r['chapter'];
+//                echo ':' . $r['verse'];
+//                echo '</a></p>';
+//            }
+
+var_dump($add_script);
+?>
+        
     </body>
 </html>
