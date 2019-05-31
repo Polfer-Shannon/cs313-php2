36ff541d683b4df30cf954064cf953a3a3cbe010
Polfer-Shannon <shannon.polfer@gmail.com>
2019-05-31 15:15:23 -0600

    working on adding a recipe

--- web/prove/views/project1/newRecipes.php
+++ web/prove/views/project1/newRecipes.php
@@ -11,26 +11,22 @@ session_start();
         $users_id = $_SESSION["users_id"];
         $add_directions = htmlspecialchars($_POST['addRecipeDirections']);
         
-        print_r($users_id);
-        var_dump($add_recipe);
-        var_dump($add_rank);
-        var_dump($add_date);
-        var_dump($add_directions);
-//        $stmt = $db->prepare('INSERT INTO recipes(name, rank, date, user_id,  directions,) VALUES(:addRecipeName, :addRank, :addDateServed, :user_id, :addRecipeDirections,);');
-//        $stmt->bindValue(':addRecipeName', $add_recipe, PDO::PARAM_STR);
-//        $stmt->bindValue(':addRank', $add_rank, PDO::PARAM_INT);
-//        $stmt->bindValue(':addDateServed', $add_date, PDO::PARAM_STR);
-//        $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
-//        $stmt->bindValue(':addRecipeDirections', $add_directions, PDO::PARAM_STR);
-//        $stmt->execute();
-//        $recipe_id = $db->lastInsertId();
-//        
-//        foreach ($menu as $m){
-//            $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipies_id, :ingredients_id);');
-//           $stmt->bindValue(':recipies_id', $recipe_id, PDO::PARAM_INT);
-//            $stmt->bindValue('ingredients_id', $m, PDO::PARAM_INT);
-//            $stmt->execute();
-//        }
+      
+        $stmt = $db->prepare('INSERT INTO recipes(name, rank, date, user_id,  directions,) VALUES(:addRecipeName, :addRank, :addDateServed, :user_id, :addRecipeDirections,);');
+        $stmt->bindValue(':addRecipeName', $add_recipe, PDO::PARAM_STR);
+        $stmt->bindValue(':addRank', $add_rank, PDO::PARAM_INT);
+        $stmt->bindValue(':addDateServed', $add_date, PDO::PARAM_STR);
+        $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
+        $stmt->bindValue(':addRecipeDirections', $add_directions, PDO::PARAM_STR);
+        $stmt->execute();
+        $recipe_id = $db->lastInsertId();
+        
+        foreach ($menu as $m){
+            $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipies_id, :ingredients_id);');
+           $stmt->bindValue(':recipies_id', $recipe_id, PDO::PARAM_INT);
+            $stmt->bindValue('ingredients_id', $m, PDO::PARAM_INT);
+            $stmt->execute();
+        }
         
        
 
