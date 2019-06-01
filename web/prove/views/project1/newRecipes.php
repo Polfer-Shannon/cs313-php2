109b63f5c04743dba758cbcde3b0ad5e568d7821
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-01 15:57:20 -0600

    still trying to figure out adding an ingredient

--- web/prove/views/project1/newRecipes.php
+++ web/prove/views/project1/newRecipes.php
@@ -17,7 +17,7 @@ $ingredients = $_POST['ingredients'];
 $newIngredient = htmlspecialchars($_POST['newIngredient']);
 $newIngredient_text = htmlspecialchars($_POST['newIngredient_text']);
 
-$stmt = $db->prepare('INSERT INTO recipes(name, rank, date, user_id, directions) VALUES(:addRecipeName, :addRank, :addDateServed, :user_id, :addRecipeDirections);');
+$stmt = $db->prepare('INSERT INTO recipes(name, rank, date, user_id, directions) VALUES(:addRecipeName, :addRank, :addDateServed, :user_id, :addRecipeDirections)');
 $stmt->bindValue(':addRecipeName', $add_recipe, PDO::PARAM_STR);
 $stmt->bindValue(':addRank', $add_rank, PDO::PARAM_INT);
 $stmt->bindValue(':addDateServed', $add_date, PDO::PARAM_STR);
@@ -28,7 +28,7 @@ $recipe_id = $db->lastInsertId();
 $_SESSION["recipe_id"] = $recipe_id;
 
 foreach ($ingredients as $i) {
-    $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipes_id, :ingredients_id);');
+    $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipes_id, :ingredients_id)');
     $stmt->bindValue(':recipes_id', $recipe_id, PDO::PARAM_INT);
     $stmt->bindValue(':ingredients_id', $i, PDO::PARAM_INT);
     $stmt->execute();
@@ -36,12 +36,12 @@ foreach ($ingredients as $i) {
 
 
 if ($newIngredient == "true"){
-    $stmt = $db->prepare('INSERT INTO ingredients(food) VALUES(:food);');
-    $stmt->bindValue(':food', $newIngredient, PDO::PARAM_STR);
+    $stmt = $db->prepare('INSERT INTO ingredients(food) VALUES(:food)');
+    $stmt->bindValue(':food', $newIngredient_text, PDO::PARAM_STR);
     $stmt->execute();
     $ingredients_id = $db->lastInsertId();
 
-    $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipes_id, :ingredients_id);');
+    $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipes_id, :ingredients_id)');
     $stmt->bindValue(':recipes_id', $recipe_id, PDO::PARAM_INT);
     $stmt->bindValue(':ingredients_id', $topic_id, PDO::PARAM_INT);
     $stmt->execute();
