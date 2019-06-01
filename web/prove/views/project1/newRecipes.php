23cd6a8757d2d689811eba8e2074df65e29e831c
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-01 02:27:26 -0600

    working on adding a recipe

--- web/prove/views/project1/newRecipes.php
+++ web/prove/views/project1/newRecipes.php
@@ -27,27 +27,27 @@ $stmt->execute();
 $recipe_id = $db->lastInsertId();
 
 $_SESSION["recipe_id"] = $recipe_id;
-$newIngredient = htmlspecialchars($_POST['newIngredient']);
-$addCategory = htmlspecialchars($_POST['addCategory']);
-$ingredients = $_POST['ingredients'];
+//$newIngredient = htmlspecialchars($_POST['newIngredient']);
+//$addCategory = htmlspecialchars($_POST['addCategory']);
+//$ingredients = $_POST['ingredients'];
 
-foreach ($ingredients as $i) {
-    $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipes_id, :ingredients_id);');
-    $stmt->bindValue(':recipes_id', $recipe_id, PDO::PARAM_INT);
-    $stmt->bindValue(':ingredients_id', $i, PDO::PARAM_INT);
-    $stmt->execute();
-}
-
-    $stmt = $db->prepare('INSERT INTO ingredients(food, category) VALUES(:food, :category)');
-    $stmt->bindValue(':food', $newIngredient, PDO::PARAM_STR);
-    $stmt->bindValue(':category', $addCategory, PDO::PARAM_STR);
-    $stmt->execute();
-    $ingredients_id = $db->lastInsertId();
-
-    $stmt = $db->prepare('INSERT INTO scrip_top(scriptures_id, topic_id) VALUES(:scriptures_id, :topic_id)');
-    $stmt->bindValue(':scriptures_id', $scripture_id, PDO::PARAM_INT);
-    $stmt->bindValue(':topic_id', $topic_id, PDO::PARAM_INT);
-    $stmt->execute();
+//foreach ($ingredients as $i) {
+//    $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipes_id, :ingredients_id);');
+//    $stmt->bindValue(':recipes_id', $recipe_id, PDO::PARAM_INT);
+//    $stmt->bindValue(':ingredients_id', $i, PDO::PARAM_INT);
+//    $stmt->execute();
+//}
+//
+//    $stmt = $db->prepare('INSERT INTO ingredients(food, category) VALUES(:food, :category)');
+//    $stmt->bindValue(':food', $newIngredient, PDO::PARAM_STR);
+//    $stmt->bindValue(':category', $addCategory, PDO::PARAM_STR);
+//    $stmt->execute();
+//    $ingredients_id = $db->lastInsertId();
+//
+//    $stmt = $db->prepare('INSERT INTO scrip_top(scriptures_id, topic_id) VALUES(:scriptures_id, :topic_id)');
+//    $stmt->bindValue(':scriptures_id', $scripture_id, PDO::PARAM_INT);
+//    $stmt->bindValue(':topic_id', $topic_id, PDO::PARAM_INT);
+//    $stmt->execute();
 
 header("Location: addRecipes.php");    
   ?>     
