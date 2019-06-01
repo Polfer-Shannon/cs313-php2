6ab815f5486d876c3abe46d2595aa6430722a3be
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-01 17:15:20 -0600

    still trying to figure out adding an ingredient

--- web/prove/views/project1/newRecipes.php
+++ web/prove/views/project1/newRecipes.php
@@ -25,7 +25,7 @@ $stmt->bindValue(':user_id', $users_id, PDO::PARAM_INT);
 $stmt->bindValue(':addRecipeDirections', $add_directions, PDO::PARAM_STR);
 $stmt->execute();
 $recipe_id = $db->lastInsertId();
-//$_SESSION["recipe_id"] = $recipe_id;
+$_SESSION["recipe_id"] = $recipe_id;
 
 foreach ($ingredients as $i) {
     $stmt = $db->prepare('INSERT INTO menu(recipes_id, ingredients_id) VALUES(:recipes_id, :ingredients_id)');
