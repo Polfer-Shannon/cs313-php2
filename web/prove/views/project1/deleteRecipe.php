09261b57911e8eefdd057672e20275bb497016e3
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-01 21:28:58 -0600

    working on css

--- web/prove/views/project1/deleteRecipe.php
+++ web/prove/views/project1/deleteRecipe.php
@@ -1,8 +1,15 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require 'dbConnect.php';
+$db = get_db();
+session_start();
+
+$id = filter_var($_GET['id']);
+$stmt = $db->prepare('DELETE FROM menu  WHERE recipes_id = ' . $id);
+$stmt->execute();
+
+$stmt = $db->prepare('DELETE FROM recipes WHERE id = ' . $id);
+$stmt->execute();
+
+header("Location: addRecipes.php"); 
 
