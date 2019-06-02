b90afcbc3e2dbda1e58256719ca0a46ed114ea5e
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-01 18:09:21 -0600

    still trying to figure out adding an ingredient

--- web/prove/views/project1/details.php
+++ web/prove/views/project1/details.php
@@ -13,10 +13,10 @@ Personal Home Page
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SP Home</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
-        <!--        <link href="../../css/home_style.css" rel="stylesheet" type="text/css" 
+                <link href="../../css/home_style.css" rel="stylesheet" type="text/css" 
                       media="screen">
                 <link href="../../css/project1.css" rel="stylesheet" type="text/css" 
-                      media="screen">-->
+                      media="screen">
         <script src="../../js/homejs.js"></script>
     </head>
     <body>
