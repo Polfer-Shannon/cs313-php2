ab42c68c1a37aee0f4e2a396f77adea02373d070
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-01 18:44:54 -0600

    working on css

--- web/prove/views/project1/addRecipes.php
+++ web/prove/views/project1/addRecipes.php
@@ -83,7 +83,7 @@ Personal Home Page
                     <input type="text" name="newIngredient_text" placeholder="Type new ingredient">
 
 
-                    <input  class="form-control btn-primary" type="submit" name="addRecipe" value="Add Recipe">
+                    <input  class="form-control btn-primary" type="submit" value="Add Recipe">
                 </div>
             </form>
 
@@ -96,23 +96,20 @@ Personal Home Page
         $stmt->bindValue(':recipes_id', $recipes_id, PDO::PARAM_INT);
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        if (isset($_POST["addRecipe"])){
         echo "<h3>" . $row['name'] . "</h3>";
         echo "<p>" . 'Family Rank: ' . $row['rank'] . "</p>";
         echo "<p>" . 'Last Served on: ' . $row['date'] . "</p>";
         echo "<h4>" . 'Directions:' . "</h4>";
         echo "<p>" . $row['directions'] . "</p>";
-        }
+
         $stmt2 = $db->prepare('SELECT ingredients.food FROM ingredients LEFT JOIN menu ON menu.ingredients_id = ingredients.id WHERE menu.recipes_id =:recipes_id');
         $stmt2->bindValue(':recipes_id', $recipes_id, PDO::PARAM_INT);
         $stmt2->execute();
         $ingredients = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-        if (isset($_POST["addRecipe"])){
         echo "<h4>" . 'Ingredient List' . "</h4>";
         foreach ($ingredients as $i){ 
             echo "<p>" . $i['food'] . "</p>";
             echo '<br>';
-        }
         }
         ?>
         </div>
