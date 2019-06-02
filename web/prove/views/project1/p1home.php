2be2a676479edce38427751536ee9c57adf2cf2f
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-01 18:53:21 -0600

    working on css

--- web/prove/views/project1/p1home.php
+++ web/prove/views/project1/p1home.php
@@ -49,7 +49,7 @@ Personal Home Page
             
             
         </div>
-        <footer class="card-footer">
+        <footer class="card-footer text-muted">
             <?php include ('../../common/footer.php'); ?>
         </footer>
     </body>
