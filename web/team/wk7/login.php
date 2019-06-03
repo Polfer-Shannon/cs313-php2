d45443f0d53c990f3ba44821dba5ded5fc107194
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-03 14:19:25 -0600

    bootstrap practice

--- web/team/wk7/login.php
+++ web/team/wk7/login.php
@@ -1,8 +1,61 @@
 <?php
+require 'dbConnect.php';
+$db = get_db();
+?>
+<!DOCTYPE html>
+<!--
+Team Week 7
+-->
+<html lang="en-us">
+    <head>  
+        <title>SP Home</title>
+        <meta charset="UTF-8"/>
+        <meta name="author" content="Shannon Polfer">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">    
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
+        <script src="../../js/homejs.js"></script>
+    </head>
+
+    <body class="text-center">
+        <header>
+
+        </header>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <a class="navbar-brand" href="#">Top navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="signUp.php">Sign up</a>
+      </li>
+      
+    </ul>
+
+  </div>
+</nav>
+        
+          <form class="form-signin">
+  <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+  <label for="inputEmail" class="sr-only">Email address</label>
+  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+  <div class="checkbox mb-3">
+    <label>
+      <input type="checkbox" value="remember-me"> Remember me
+    </label>
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+</form>
+    </body>
+</html>
 
