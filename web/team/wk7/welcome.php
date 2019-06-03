044df5913693370763e7b22d7d1b1d29cf26e604
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-03 13:54:25 -0600

    setting up bootstrap as download

--- web/team/wk7/welcome.php
+++ web/team/wk7/welcome.php
@@ -12,15 +12,15 @@ Team Week 7
         <meta charset="UTF-8"/>
         <meta name="author" content="Shannon Polfer">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<!--        <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">-->
+        
+        <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
         <script src="../../js/homejs.js"></script>
     </head>
 
     <body>
         <header>
 <nav class="navbar navbar-default">
-  <div class="container-fluid">
+  <div>
     <div class="navbar-header">
       <a class="navbar-brand" href="#">WebSiteName</a>
     </div>
