c787d5bedd443eecf7c6607cc234812e5496caf1
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-03 13:59:58 -0600

    setting up bootstrap as download

--- web/team/wk7/welcome.php
+++ web/team/wk7/welcome.php
@@ -12,27 +12,28 @@ Team Week 7
         <meta charset="UTF-8"/>
         <meta name="author" content="Shannon Polfer">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-        
+
         <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
         <script src="../../js/homejs.js"></script>
     </head>
 
     <body>
         <header>
-<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav>
+
         </header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">WebSiteName</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+            </div>
+        </nav>
     </body>
 </html>
 
