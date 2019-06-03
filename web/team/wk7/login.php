d0f6aa428ee1a04b6f1feebe94fb6d69dd8155e1
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-03 14:39:20 -0600

    bootstrap practice

--- web/team/wk7/login.php
+++ web/team/wk7/login.php
@@ -28,18 +28,7 @@ Team Week 7
     <span class="navbar-toggler-icon"></span>
   </button>
   
-  <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link" href="signUp.php">Sign up</a>
-      </li>
-      
-    </ul>
-
-  </div>
+ 
 </nav>
         
           <form class="form-signin">
