620dd52426803080a2b8855341e055e790e39133
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-03 19:50:27 -0600

    Working on wk7 team

--- web/team/wk7/login.php
+++ web/team/wk7/login.php
@@ -21,37 +21,37 @@ Team Week 7
         <header>
 
         </header>
-<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-  <a class="navbar-brand" href="../../prove/views/assignments.php">Assignments Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  
- <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav mr-auto">
-     
-      <li class="nav-item">
-          <a class="nav-link" href="signUp.php">Sign up</a>
-      </li>
-      
-    </ul>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+            <a class="navbar-brand" href="../../prove/views/assignments.php">Assignments Home</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-  </div>
-</nav>
-        
-          <form class="form-signin">
-              <div class="form-group">
-  <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-  <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-  <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-  
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-              </div>
-              
-</form>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="signUp.php">Sign up</a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </nav>
+
+        <form class="form-signin">
+            <div class="form-group">
+                <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+                <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <label for="username" class="sr-only">Username</label>
+                <input type="text" name="username" id="inputEmail" class="form-control" placeholder="Username">
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </div>
+
+        </form>
     </body>
 </html>
 
--- web/team/wk7/signUp.php
+++ web/team/wk7/signUp.php
@@ -20,23 +20,38 @@ Team Week 7
 
     <body class="text-center">
         <header>
-
         </header>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
     <a class="navbar-brand" href="../../prove/views/assignments.php">Assignments Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  
   <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav mr-auto">
-      
+    <ul class="navbar-nav mr-auto">   
       <li class="nav-item">
           <a class="nav-link" href="login.php">Login</a>
-      </li>
-      
+      </li>     
     </ul>
-
   </div>
 </nav>
+        
+        <form class="form-signin"  method="post" action="/acme/accounts/index.php">
+                    <div class="form-group">
+                        <h1 class=" h3 mb-3 font-weight-normal">Account Sign Up</h1>
 
+                        <label class="sr-only" for="username">Enter a Username:</label>
+                        <input class="form-control" type="text" name="username" id="username" placeholder="Enter a Username"><br>
+                        <label class="sr-only" for="clientPassword">Password:</label></h2> 
+                        <input class="form-control" type="password" name="clientPassword" id="clientPassword" placeholder="Required" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"><br>    
+                    
+                    <p class="login">Passwords must be at least 8 character's and contain at least 1<br> of each of the following: number, capital letter, special character.</p>
+                    <input class="form-control" type="submit" name="submit" id="regbutton" value="Register">
+                    <!--Add the action name - value pair -->
+                    <input type="hidden" name="action" value="register">                        
+
+                    </div>
+                </form>
+        
+        
+    </body>
+</html>
