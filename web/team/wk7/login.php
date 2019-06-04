ed9a950eceaf08d77ca611b48ab3c04aa54561eb
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-03 19:58:27 -0600

    Working on wk7 team

--- web/team/wk7/login.php
+++ web/team/wk7/login.php
@@ -41,7 +41,7 @@ Team Week 7
 
         <form class="form-signin">
             <div class="form-group">
-                <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+                
                 <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
                 <label for="username" class="sr-only">Username</label>
                 <input type="text" name="username"  class="form-control" placeholder="Username">
