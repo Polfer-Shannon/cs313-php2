a717577ee76a55500c77fc74669eb00950a59c42
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-04 19:15:17 -0600

    working on team wk7 assignment

--- web/team/wk7/accountIndex.php
+++ web/team/wk7/accountIndex.php
@@ -3,15 +3,18 @@ require 'dbConnect.php';
 $db = get_db();
 
 session_start();
+?>
 
-
+<?php
 //Get input from signup form
 $username = htmlspecialchars($_POST['username']);
 $password = htmlspecialchars($_POST['password']);
 
-$stmt = $db->prepare('INSERT INTO client(username, password VALUES ):username, :password');
+$stmt = $db->prepare('INSERT INTO client(username, password) VALUES(:username, :password)');
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
 $stmt->bindValue(':password', $password, PDO::PARAM_STR);
 $stmt->execute();
 
 header("Location: login.php"); 
+
+?>
