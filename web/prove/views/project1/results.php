f76e03ae9adb0805c0920872b89f26f27eca7b61
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-07 15:38:01 -0600

    fixing delete recipe

--- web/prove/views/project1/results.php
+++ web/prove/views/project1/results.php
@@ -52,7 +52,7 @@ Personal Home Page
                 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 echo '<table>';
                 foreach ($rows as $r) {
-                    echo '<tr><td><span><a href="details.php?recipeLinks=' . $r['id'] . '">' . $r['name'] . '</a></span></td>';
+                    echo '<tr><td><span><a class="recipes" href="details.php?recipeLinks=' . $r['id'] . '">' . $r['name'] . '</a></span></td>';
                    
                     echo '<td><a class="btn btn-primary" href="deleteRecipe.php?id=' . $r['id'] . '">' . 'Delete' . '</a></td></tr>';
                    
