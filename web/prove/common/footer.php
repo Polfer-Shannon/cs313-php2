7d53ab0329702fe8790c4d6eeee4e48c406f8e19
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-08 19:01:57 -0600

    working on project

--- web/prove/common/footer.php
+++ web/prove/common/footer.php
@@ -1,4 +1,4 @@
 <div class="card-footer text-center footer-bg_color foot">
     <p>&copy; Shannon Polfer, All rights reserved.</p>
-    <p>All images used are the property of Shannon Polfer, and/or are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.<p>
+    <p>All images used are the property of Shannon Polfer, and/or are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
 </div>
