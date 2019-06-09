183dbfa9cde5d632104be36ba5957c8602628585
Polfer-Shannon <shannon.polfer@gmail.com>
2019-06-08 20:35:21 -0600

    working on project

--- web/prove/project1/views/newIngredients.php
+++ web/prove/project1/views/newIngredients.php
@@ -68,6 +68,10 @@ New Ingredients page
                         <label for="newIngredient">New Ingredient:</label> 
                         <input type="checkbox" name="newIngredient" value="true">
                         <input type="text" name="newIngredient_text" placeholder="Type new ingredient">
+                        <br>
+                        <label for="newIngredient">New Ingredient:</label> 
+                        <input type="checkbox" name="newIngredient" value="true">
+                        <input type="text" name="newIngredient_text" placeholder="Type new ingredient">
 
                         <input  class="form-control btn-primary" type="submit" value="Add Ingredients">
                     </div>
